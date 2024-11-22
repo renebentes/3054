f8ae4159f306b057f3648a6a7ec1cced047f0dad
Rene Bentes Pinto <renebentes@yahoo.com.br>
2024-11-22 18:58:06 -0300

    feat: define classe abstrata Entity
    Essa classe é responsável por definir características
    e comportamentos comuns às entidades

--- src/Dima.Core/Primitives/Entity.cs
+++ src/Dima.Core/Primitives/Entity.cs
@@ -1,6 +1,26 @@
 namespace Dima.Core.Primitives;
 
-internal abstract class Entity
+public abstract class Entity
+
 {
-    public long Id { get; }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Entity"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Required by EF Core.
+    /// </remarks>
+    protected Entity()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Entity"/> class.
+    /// </summary>
+    /// <param name="id">The entity identifier.</param>
+    protected Entity(long id)
+    {
+        Id = id;
+    }
+
+    public long Id { get; private init; }
 }
