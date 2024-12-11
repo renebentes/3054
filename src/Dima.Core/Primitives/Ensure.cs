e02ebd6fc39237c7c78b97fd87f352e61430e91c
Rene Bentes Pinto <renebentes@yahoo.com.br>
2024-12-11 18:04:36 -0300

    feat(Ensure): define tipo para englobar validações comuns de objetos

---
+++ src/Dima.Core/Primitives/Ensure.cs
@@ -0,0 +1,36 @@
+namespace Dima.Core.Primitives;
+
+/// <summary>
+/// Contains assertions for the most common application checks.
+/// </summary>
+public static class Ensure
+{
+    /// <summary>
+    /// Ensures that specified <see cref="string"/> <paramref name="input"/> is not longer than <paramref name="maxLength"/>
+    /// </summary>
+    /// <param name="input">The input to check</param>
+    /// <param name="maxLength">The maximum length</param>
+    /// <param name="error">The <see cref="Error"/> object to display on failure.</param>
+    /// <exception cref="DomainException">Thrown if <paramref name="input"/> is longer than <paramref name="maxLength"/></exception>
+    public static void MaxLength(string input, int maxLength, Error error)
+    {
+        if (input.Length > maxLength)
+        {
+            throw new DomainException(error);
+        }
+    }
+
+    /// <summary>
+    /// Ensures that the specified <see cref="string"/> <paramref name="input"/> is not empty.
+    /// </summary>
+    /// <param name="input">The input to check</param>
+    /// <param name="error">The <see cref="Error"/> object to display on failure.</param>
+    /// <exception cref="DomainException">Thrown if <paramref name="input"/> is <see langword="null"/> or empty.</exception>
+    public static void NullOrEmpty(string input, Error error)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            throw new DomainException(error);
+        }
+    }
+}
