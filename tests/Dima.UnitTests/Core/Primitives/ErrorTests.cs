df26afa74bc0f24aef8d8a1272d315d7dbbaea33
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-01-22 18:15:58 -0300

    test: altera padrão de nomenclatura dos métodos de teste

--- tests/Dima.UnitTests/Core/Primitives/ErrorTests.cs
+++ tests/Dima.UnitTests/Core/Primitives/ErrorTests.cs
@@ -5,7 +5,7 @@ namespace Dima.UnitTests.Core.Primitives;
 public class ErrorTests
 {
     [Fact]
-    public void ImplicitOperatorShouldReturnCodeFromError()
+    public void ImplicitConversion_ReturnsErrorCode()
     {
         // Arrange
         var error = new Error("Error.Test", "Error description");
@@ -18,7 +18,7 @@ public class ErrorTests
     }
 
     [Fact]
-    public void ToStringShouldReturnMessageFromError()
+    public void ToString_ReturnsErrorMessage()
     {
         // Arrange
         var error = new Error("Error.Test", "Error description");
@@ -28,5 +28,27 @@ public class ErrorTests
 
         // Assert
         result.Should().Be(error.Message);
+    }
+
+    [Fact]
+    public void TwoDifferentErrors_AreNotEqual()
+    {
+        // Arrange
+        var error1 = new Error("Error.404", "Not Found");
+        var error2 = new Error("Error.500", "Internal Server Error");
+
+        // Act & Assert
+        error1.Should().NotBe(error2);
+    }
+
+    [Fact]
+    public void TwoIdenticalErrors_AreEqual()
+    {
+        // Arrange
+        var error1 = new Error("error", "Error description");
+        var error2 = new Error("error", "Error description");
+
+        // Act & Assert
+        error1.Should().Be(error2);
     }
 }
