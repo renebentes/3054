4e2d61542843c0cb4ba7f1764f6231706ded3650
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-03-17 21:56:18 -0300

    docs(CreateCategory): adiciona comentários de documentação XML
    Adiciona documentação XML à classe CreateCategoryHandler e seus métodos
    para melhorar a clareza e a manutenibilidade do código. Além disso, in-
    clui documentação para a interface ICreateCategoryHandler para definir
    seu papel.
    
    Atualiza a instanciação do CreateCategoryRequest nos testes para utilizar
    o operador de não-nulidade no parâmetro title, indicando que ele não
    será nulo.

--- src/Dima.Api/Categories/CreateCategory/CreateCategoryHandler.cs
+++ src/Dima.Api/Categories/CreateCategory/CreateCategoryHandler.cs
@@ -4,11 +4,23 @@ using FluentValidation;
 
 namespace Dima.Api.Categories.CreateCategory;
 
+
+/// <summary>
+/// Handles the creation of a new category.
+/// </summary>
+/// <param name="context">The database context.</param>
+/// <param name="validator">The validator for the create category request.</param>
 internal sealed class CreateCategoryHandler(
     DimaDbContext context,
     IValidator<CreateCategoryRequest> validator)
     : ICreateCategoryHandler
 {
+    /// <summary>
+    /// Handles the create category request asynchronously.
+    /// </summary>
+    /// <param name="request">The create category request.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A result containing the ID of the created category.</returns>
     public async Task<Result<long>> HandleAsync(
         CreateCategoryRequest request,
         CancellationToken cancellationToken)
--- src/Dima.Core/Categories/CreateCategory/ICreateCategoryHandler.cs
+++ src/Dima.Core/Categories/CreateCategory/ICreateCategoryHandler.cs
@@ -1,5 +1,8 @@
 namespace Dima.Core.Categories.CreateCategory;
 
+/// <summary>
+/// Defines a handler for creating a category.
+/// </summary>
 public interface ICreateCategoryHandler : IRequestHandler<CreateCategoryRequest, Result<long>>
 {
 }
