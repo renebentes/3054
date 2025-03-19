e8b445ec49b5537365f8e006fd82ccc235c7439b
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-03-18 22:38:43 -0300

    style: simplifica código
    Atualiza o nível de severidade para o diagnóstico IDE1006 para aviso no
    arquivo .editorconfig e corrige um erro de digitação nos comentários dos
    analisadores do Roslynator.
    
    A severidade para a regra RCS1194 é definida como nenhuma, e as configurações
    para quebra de argumentos e cadeias de chamadas foram atualizadas.
    
    Em `20250120201048_InitialCreate.cs`, simplifica a definição da chave primária
    removendo chaves desnecessárias. Alterações semelhantes são feitas nas definições
    de interface em `ICreateCategoryHandler.cs` e `IRequest.cs`, aprimorando a
    legibilidade do código.

--- src/Dima.Core/Messaging/IRequest.cs
+++ src/Dima.Core/Messaging/IRequest.cs
@@ -4,6 +4,4 @@ namespace Dima.Core.Messaging;
 /// Represents a request with a response.
 /// </summary>
 /// <typeparam name="TResponse">A response type</typeparam>
-public interface IRequest<out TResponse>
-{
-}
+public interface IRequest<out TResponse>;
