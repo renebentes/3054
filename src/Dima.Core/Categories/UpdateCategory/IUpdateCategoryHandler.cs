5860a6efafc2c438eb8fab8069317f7ffa76bd5f
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-03-18 22:42:55 -0300

    feat(category): adiciona funcionalidade para atualizar categorias
    Introduz um novo namespace `Dima.Core.Categories.UpdateCategory` para
    organizar o código relacionado à atualização de categorias. Isso inclui
    a criação da interface `IUpdateCategoryHandler` para lidar com as
    solicitações de atualização, o registro `UpdateCategoryRequest` para
    encapsular os dados necessários para atualizações e o
    `UpdateCategoryRequestValidator` para impor regras de validação nos da-
    dos da solicitação.

---
+++ src/Dima.Core/Categories/UpdateCategory/IUpdateCategoryHandler.cs
@@ -0,0 +1,7 @@
+namespace Dima.Core.Categories.UpdateCategory;
+
+/// <summary>
+/// Defines a handler for updating a category.
+/// </summary>
+public interface IUpdateCategoryHandler
+    : IRequestHandler<UpdateCategoryRequest, Result>;
