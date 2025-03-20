df550ac5ff9fc60f5fc44c2fd21a72b8885e42df
Rene B. Pinto <renebentes@yahoo.com.br>
2025-03-20 09:59:34 -0300

    feat(Category): adiciona funcionalidade de atualização
    Implementa a funcionalidade de atualização de categorias, adicionando
    um manipulador para atualizar categorias com validação e tratamento de
    erros. Além disso, estende os tipos de resultado para incluir os status
    NotFound e NoContent e aprimora os erros de domínio com mensagens de er-
    ro específicas de categoria.
    
    Por fim, define um método de conversão implícita para a classe Title.

---
+++ src/Dima.Api/Categories/UpdateCategory/UpdateCategoryHandler.cs
@@ -0,0 +1,61 @@
+using Dima.Api.Common;
+using Dima.Core.Categories;
+using Dima.Core.Categories.UpdateCategory;
+using FluentValidation;
+
+namespace Dima.Api.Categories.UpdateCategory;
+
+/// <summary>
+/// Handles the update category operations.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="UpdateCategoryHandler"/> class.
+/// </remarks>
+/// <param name="context">The database context.</param>
+/// <param name="validator">The validator for the update category request.</param>
+public class UpdateCategoryHandler(
+    DimaDbContext context,
+    IValidator<UpdateCategoryRequest> validator)
+    : IUpdateCategoryHandler
+{
+    /// <summary>
+    /// Handles the update category request asynchronously.
+    /// </summary>
+    /// <param name="request">The update category request.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the result of the operation.</returns>
+    public async Task<Result> HandleAsync(
+        UpdateCategoryRequest request,
+        CancellationToken cancellationToken)
+    {
+        var validatorResult = await validator.ValidateAsync(
+            request,
+            cancellationToken);
+
+        if (!validatorResult.IsValid)
+        {
+            return Result.Invalid(
+                validatorResult
+                    .AsErrors()
+            );
+        }
+
+        var category = await context.Categories
+            .FindAsync(
+            [request.Id],
+            cancellationToken: cancellationToken);
+
+        if (category is null)
+        {
+            return Result.NotFound(DomainErrors.Category.NotFound);
+        }
+
+        category.ChangeTitle(request.Title);
+        category.ChangeDescription(request.Description);
+
+        context.Update(category);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
