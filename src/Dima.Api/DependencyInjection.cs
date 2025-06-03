72bd2facf59bce834aaba0e443dd7d8b4ff6fb62
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-06-02 21:38:34 -0300

    feat(DI): melhore a manipulação de categorias
    Adicione novas diretivas `using` para manipuladores de atualização de categorias e consolide o
    registro de manipuladores no método `AddApi`. O método `AddDocumentation` foi atualizado para
    incluir a geração do Swagger, melhorando a documentação da API. Um novo método privado
    `AddHandlers` foi introduzido para registrar tanto os manipuladores de criação quanto de
    atualização de categorias.

--- src/Dima.Api/DependencyInjection.cs
+++ src/Dima.Api/DependencyInjection.cs
@@ -1,5 +1,7 @@
 using Dima.Api.Categories.CreateCategory;
+using Dima.Api.Categories.UpdateCategory;
 using Dima.Core.Categories.CreateCategory;
+using Dima.Core.Categories.UpdateCategory;
 
 namespace Dima.Api;
 
@@ -15,9 +17,9 @@ internal static class DependencyInjection
     /// <returns>The <see cref="IServiceCollection"/> with the added services.</returns>
     internal static IServiceCollection AddApi(this IServiceCollection services)
     {
-        services.AddDocumentation();
-
-        services.AddTransient<ICreateCategoryHandler, CreateCategoryHandler>();
+        services
+            .AddDocumentation()
+            .AddHandlers();
 
         return services;
     }
@@ -29,8 +31,22 @@ internal static class DependencyInjection
     /// <returns>The <see cref="IServiceCollection"/> with the added services.</returns>
     private static IServiceCollection AddDocumentation(this IServiceCollection services)
     {
-        services.AddEndpointsApiExplorer()
+        services
+            .AddEndpointsApiExplorer()
             .AddSwaggerGen();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers handler implementations for operations in the service collection.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to which the handlers will be added.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/> with the registered handlers.</returns>
+    private static IServiceCollection AddHandlers(this IServiceCollection services)
+    {
+        services.AddTransient<ICreateCategoryHandler, CreateCategoryHandler>();
+        services.AddTransient<IUpdateCategoryHandler, UpdateCategoryHandler>();
 
         return services;
     }
