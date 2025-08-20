52742a40728a153294dd4dc1ca5341cf17c89793
Rene Bentes Pinto <renebentes@yahoo.com.br>
2025-08-20 00:51:54 -0300

    fix(UI): atualize o título da página e traduza mensagem de error

--- src/Dima.WebApp/Program.cs
+++ src/Dima.WebApp/Program.cs
@@ -6,8 +6,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+builder
+    .RootComponents
+    .Add<App>("#app");
+builder
+    .RootComponents
+    .Add<HeadOutlet>("head::after");
 
 builder
     .Services
